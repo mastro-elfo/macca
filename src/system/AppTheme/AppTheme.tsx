9210d270af7c3dbe32cb6eb666c2b627fb8c173e
Francesco <francesco.209@gmail.com>
2023-12-16 23:31:29 +0100

    Fix app theme

--- src/system/AppTheme/AppTheme.tsx
+++ src/system/AppTheme/AppTheme.tsx
@@ -4,6 +4,8 @@ import {
   ThemeProvider,
   useMediaQuery,
 } from "@mui/material";
+import primary from "@mui/material/colors/blue";
+import secondary from "@mui/material/colors/pink";
 import { enUS as muiEnUs, itIT as muiItIt } from "@mui/material/locale";
 import { PropsWithChildren, useMemo } from "react";
 import { useTranslation } from "react-i18next";
@@ -12,56 +14,19 @@ export default function AppTheme({ children }: PropsWithChildren) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const locales = useLocales();
 
-  const themeUtility = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light",
-        },
-      }),
-    [prefersDarkMode]
-  );
-
   const theme = useMemo(
     () =>
       createTheme(
-        themeUtility,
         {
           palette: {
-            primary: themeUtility.palette.augmentColor({
-              color: { main: "#6da22f" },
-              name: "HubPrimary",
-            }),
-            secondary: themeUtility.palette.augmentColor({
-              color: { main: "#142c47" },
-              name: "HubSecondary",
-            }),
-          },
-          components: {
-            MuiListItemText: {
-              defaultProps: {
-                primaryTypographyProps: {
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                },
-              },
-            },
-            MuiDialog: {
-              defaultProps: {
-                maxWidth: "lg",
-              },
-            },
-            MuiAppBar: {
-              defaultProps: {
-                color: "primary",
-                // enableColorOnDark: true,
-              },
-            },
+            mode: prefersDarkMode ? "dark" : "light",
+            primary,
+            secondary,
           },
         },
         ...locales
       ),
-    [themeUtility, locales]
+    [prefersDarkMode, locales]
   );
 
   return (
