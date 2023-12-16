3c4f57f7ff1978f0c4650a4444b93ffe6e625a5d
Francesco <francesco.209@gmail.com>
2023-12-16 23:31:21 +0100

    Update routes

--- src/system/AppRoutes.tsx
+++ src/system/AppRoutes.tsx
@@ -1,7 +1,33 @@
-import { Routes } from "react-router-dom";
+import { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import FallbackLayout from "../layouts/FallbackLayout/FallbackLayout";
 
-// Generic pages
+const MapPage = lazy(() => import("../features/map/MapPage/MapPage"));
+const ArtworkListPage = lazy(
+  () => import("../features/artwork/ArtworkListPage/ArtworkListPage")
+);
+const ArtworkDetailPage = lazy(
+  () => import("../features/artwork/ArtworkDetailPage/ArtworkDetailPage")
+);
 
 export function AppRoutes() {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="" element={<Navigate to="/map" />} />
+      <Route path="map">
+        <Route path="" element={<Navigate to="/map/43.5453/10.7194/13" />} />
+        <Route
+          path=":lat/:lng"
+          element={<Navigate to="18" relative="path" />}
+        />
+        <Route path=":lat/:lng/:zoom" element={<MapPage />} />
+        <Route path=":lat/:lng/:zoom/:id" element={<MapPage />} />
+      </Route>
+      <Route path="artworks">
+        <Route path="" element={<ArtworkListPage />} />
+        <Route path=":id" element={<ArtworkDetailPage />} />
+      </Route>
+      <Route path="fallback" element={<FallbackLayout />} />
+    </Routes>
+  );
 }
