7031a435cf13803a57ea7dc3530a86150ba4b585
Francesco <francesco.209@gmail.com>
2023-12-16 23:29:49 +0100

    New component `DrawerContent`

---
+++ src/features/drawer/DrawerContent/DrawerContent.tsx
@@ -0,0 +1,39 @@
+import {
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useLocation, useNavigate } from "react-router-dom";
+import ArtworkIcon from "../../artwork/ArtworkIcon/ArtworkIcon";
+import MapIcon from "../../map/MapIcon/MapIcon";
+
+export default function DrawerContent() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <List disablePadding>
+      <ListItemButton
+        onClick={() => navigate("/")}
+        selected={location.pathname.startsWith("/map")}
+      >
+        <ListItemIcon>
+          <MapIcon />
+        </ListItemIcon>
+        <ListItemText>{t("Map")}</ListItemText>
+      </ListItemButton>
+      <ListItemButton
+        onClick={() => navigate("/artworks")}
+        selected={location.pathname.startsWith("/artworks")}
+      >
+        <ListItemIcon>
+          <ArtworkIcon />
+        </ListItemIcon>
+        <ListItemText>{t("Artworks")}</ListItemText>
+      </ListItemButton>
+    </List>
+  );
+}
