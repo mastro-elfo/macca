6a6d04425622a5e7ecb5535b1715e51dfe34e8dd
Francesco <francesco.209@gmail.com>
2023-12-23 09:49:37 +0100

    New component `BackButton`

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,22 @@
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button, ButtonProps } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+type BackButtonProps = Omit<ButtonProps, "onClick" | "startIcon">;
+
+export default function BackButton(props: BackButtonProps) {
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      {...props}
+      title={t("Back")}
+      startIcon={<ArrowBackIcon />}
+      onClick={() => navigate(-1)}
+    >
+      {t("Back")}
+    </Button>
+  );
+}
