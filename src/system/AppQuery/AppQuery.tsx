ed8b0f6a22160dbe79ef4a3baf7efce917253a6f
Francesco <francesco.209@gmail.com>
2024-01-06 10:29:10 +0100

    Artwork "In Peccioli Lux" by "Sara Marioli"

--- src/system/AppQuery/AppQuery.tsx
+++ src/system/AppQuery/AppQuery.tsx
@@ -1,13 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // refetchOnWindowFocus: false,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 export default function AppQuery({ children }: PropsWithChildren) {
   return (
