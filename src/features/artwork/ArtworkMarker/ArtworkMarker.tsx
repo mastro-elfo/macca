04808585f92627d700ce2605436564d57d36f785
Francesco <francesco.209@gmail.com>
2024-01-28 12:07:45 +0100

    Add `ArtworkPopup` component

--- src/features/artwork/ArtworkMarker/ArtworkMarker.tsx
+++ src/features/artwork/ArtworkMarker/ArtworkMarker.tsx
@@ -1,12 +1,11 @@
-import { Stack, Typography, useTheme } from "@mui/material";
+import { useTheme } from "@mui/material";
 import L from "leaflet";
 import { useEffect, useMemo, useRef } from "react";
 import { renderToString } from "react-dom/server";
-import { Marker, Popup } from "react-leaflet";
+import { Marker } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
-import { useAuthorFullNameFormatter } from "../../author/authorService";
-import ArtworkDetailButton from "../ArtworkDetailButton/ArtworkDetailButton";
 import ArtworkIcon from "../ArtworkIcon/ArtworkIcon";
+import ArtworkPopup from "../ArtworkPopup/ArtworkPopup";
 import { ArtworkEntity } from "../artworkModel";
 
 type ArtworkMarkerProps = {
@@ -21,8 +20,6 @@ export default function ArtworkMarker({
   const theme = useTheme();
   const navigate = useNavigate();
   const markerRef = useRef<L.Marker>(null);
-
-  const fullNameFormatter = useAuthorFullNameFormatter();
 
   const handleClick = () => {
     navigate(`/map/${artwork.latitude}/${artwork.longitude}/18/${artwork.id}`);
@@ -63,26 +60,7 @@ export default function ArtworkMarker({
       }}
       ref={markerRef}
     >
-      {/* TODO: ArtworkPopup */}
-      <Popup offset={[0, -24]} autoPan={false}>
-        <Stack direction="row" spacing={2}>
-          <Typography variant="h6" component="div">
-            {artwork.title}
-          </Typography>
-        </Stack>
-        <Stack direction="row" justifyContent="space-between">
-          <Typography component="div">
-            {artwork.authors.map(fullNameFormatter).join(", ")}
-          </Typography>
-          <Typography component="div">{artwork.year}</Typography>
-        </Stack>
-        <Typography variant="body2" component="div">
-          {artwork.town} - {artwork.address}
-        </Typography>
-        <Stack direction="row" justifyContent="flex-end">
-          <ArtworkDetailButton artwork={artwork} size="small" />
-        </Stack>
-      </Popup>
+      <ArtworkPopup artwork={artwork} />
     </Marker>
   );
 }
