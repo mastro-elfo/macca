75e2b460f7b107a287fb6485602ce10fc824ee5e
Francesco <francesco.209@gmail.com>
2024-02-14 21:04:11 +0100

    New component `ArtworkImageDialog`

---
+++ src/features/artwork/ArtworkImageDialog/ArtworkImageDialog.tsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogProps,
+  DialogTitle,
+  Stack,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { ArtworkEntity, ArtworkImage } from "../artworkModel";
+
+type ArtworkImageDialogProps = DialogProps & {
+  artwork?: ArtworkEntity;
+  image?: ArtworkImage | null;
+};
+
+export default function ArtworkImageDialog({
+  artwork,
+  image,
+  onClose,
+  ...props
+}: ArtworkImageDialogProps) {
+  const { t } = useTranslation();
+
+  return (
+    <Dialog fullScreen onClose={onClose} {...props}>
+      <DialogTitle>{image?.attribution.title}</DialogTitle>
+      <DialogContent>
+        <Stack height="100%" justifyContent="center">
+          <img
+            src={`/macca/media/${image?.path}`}
+            alt={t("artwork-image-alt", { ...artwork })}
+          />
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={(event) => onClose?.(event, "escapeKeyDown")}>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
