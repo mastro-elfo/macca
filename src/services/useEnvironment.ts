12a703da0e7c7b678e7275d0837fb1c666d7ace1
Francesco <francesco.209@gmail.com>
2024-02-18 16:19:49 +0100

    Fix types on useEnvironment

--- src/services/useEnvironment.ts
+++ src/services/useEnvironment.ts
@@ -3,18 +3,19 @@ export default function useEnvironment() {
   const isProduction = import.meta.env.PROD;
   const mode = import.meta.env.MODE;
 
-  const backendUrl: string = import.meta.env.VITE_BACKEND_URL;
-  const documentTitlePrefix: string =
-    import.meta.env.VITE_DOCUMENT_TITLE_PREFIX ?? "";
+  const backendUrl = String(import.meta.env.VITE_BACKEND_URL);
+  const documentTitlePrefix = String(
+    import.meta.env.VITE_DOCUMENT_TITLE_PREFIX
+  );
 
   const logDebug: boolean =
-    (import.meta.env.VITE_LOG_DEBUG ?? "").toLowerCase() === "true";
+    String(import.meta.env.VITE_LOG_DEBUG).toLowerCase() === "true";
   const logInfo: boolean =
-    (import.meta.env.VITE_LOG_INFO ?? "").toLowerCase() === "true";
+    String(import.meta.env.VITE_LOG_INFO).toLowerCase() === "true";
   const logWarning: boolean =
-    (import.meta.env.VITE_LOG_WARNING ?? "").toLowerCase() === "true";
+    String(import.meta.env.VITE_LOG_WARNING).toLowerCase() === "true";
   const logError: boolean =
-    (import.meta.env.VITE_LOG_ERROR ?? "").toLowerCase() === "true";
+    String(import.meta.env.VITE_LOG_ERROR).toLowerCase() === "true";
 
   return {
     isDevelopment,
