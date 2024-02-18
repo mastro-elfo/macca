36d3592e4c33dfb8cd460e637b83f8004b64dc6f
Francesco <francesco.209@gmail.com>
2024-02-18 16:20:05 +0100

    Fix lint on useDebounce

--- src/services/useDebounce.ts
+++ src/services/useDebounce.ts
@@ -1,14 +1,16 @@
 import { debounce } from "lodash";
 import { useEffect } from "react";
 
-export default function useDebounce<TData = any>(
-  callback: (arg: TData) => any,
+export default function useDebounce<TData = unknown, TReturn = unknown>(
+  callback: (arg: TData) => TReturn,
   arg: TData,
   wait?: number
 ) {
   useEffect(() => {
     const debounced = debounce(callback, wait);
     debounced(arg);
-    return debounced.cancel;
-  }, [arg]);
+    return () => {
+      debounced.cancel();
+    };
+  }, [arg, callback, wait]);
 }
