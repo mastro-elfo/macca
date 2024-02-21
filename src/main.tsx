3144fe7797563f4b44c9a3db36c6e5a7c51e7a24
Francesco <francesco.209@gmail.com>
2024-02-21 20:39:19 +0100

    Fix @typescript-eslint/no-non-null-assertion

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
