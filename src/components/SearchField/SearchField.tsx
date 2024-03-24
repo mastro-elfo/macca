ef03b8a25ae877172015e1f14003fcc908771b21
Francesco <francesco.209@gmail.com>
2024-03-24 11:11:53 +0100

    New component `SearchField`

---
+++ src/components/SearchField/SearchField.tsx
@@ -0,0 +1,68 @@
+import CancelIcon from "@mui/icons-material/Cancel";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  IconButton,
+  InputAdornment,
+  TextField,
+  TextFieldProps,
+} from "@mui/material";
+import { deburr } from "lodash";
+import { ChangeEventHandler, useState } from "react";
+import { useTranslation } from "react-i18next";
+import useDebounce from "../../services/useDebounce";
+type SearchfieldProps = Omit<
+  TextFieldProps,
+  "children" | "onChange" | "select"
+> & {
+  onSearch?: (deburred: string, clean: string) => void;
+};
+
+export default function SearchField({
+  defaultValue,
+  InputProps,
+  onSearch,
+  ...props
+}: SearchfieldProps) {
+  const { t } = useTranslation();
+
+  const [value, setValue] = useState<string>(
+    typeof defaultValue === "string" ? defaultValue : ""
+  );
+
+  useDebounce<{ deburred: string; clean: string }>(
+    (data) => onSearch?.(data.deburred, data.clean),
+    { deburred: deburr(value), clean: value },
+    value ? 250 : 0
+  );
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleCancel = () => {
+    setValue("");
+  };
+
+  return (
+    <TextField
+      value={value}
+      onChange={handleChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton edge="end" title={t("Clear")} onClick={handleCancel}>
+              <CancelIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
+        ...InputProps,
+      }}
+      {...props}
+    />
+  );
+}
