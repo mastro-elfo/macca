9e07d18fcd38fa92d96c89435f2745834f809bf4
Francesco <francesco.209@gmail.com>
2024-04-07 19:39:48 +0200

    New component `Cross`

---
+++ src/features/map/Cross/Cross.tsx
@@ -0,0 +1,32 @@
+import { Box, alpha } from "@mui/material";
+
+export default function Cross() {
+  return (
+    <>
+      <Box
+        width={100}
+        height={5}
+        borderRadius={5}
+        bgcolor={(theme) => alpha(theme.palette.primary.main, 0.5)}
+        position="fixed"
+        top="50%"
+        left="50%"
+        marginTop="-2.5px"
+        marginLeft="-50px"
+        zIndex={(theme) => theme.zIndex.appBar - 1}
+      />
+      <Box
+        width={5}
+        height={100}
+        borderRadius={5}
+        bgcolor={(theme) => alpha(theme.palette.primary.main, 0.5)}
+        position="fixed"
+        top="50%"
+        left="50%"
+        marginTop="-50px"
+        marginLeft="-2.5px"
+        zIndex={(theme) => theme.zIndex.appBar - 1}
+      />
+    </>
+  );
+}
