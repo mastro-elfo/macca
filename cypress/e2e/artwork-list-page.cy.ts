8dcb1d0989e68e87c5d52e393a65c5d1e38fe645
Francesco <francesco.209@gmail.com>
2024-05-05 12:42:37 +0200

    Update e2e tests [skip ci]

--- cypress/e2e/artwork-list-page.cy.ts
+++ cypress/e2e/artwork-list-page.cy.ts
@@ -4,4 +4,10 @@ describe("Artwork list page", () => {
   it("should be visible", () => {
     cy.visit("#/artworks");
   });
+
+  it("should open the detail page", () => {
+    cy.visit("#/artworks");
+    cy.contains("Detail").first().click();
+    cy.location("hash").should("match", /artworks\/\d+/);
+  });
 });
--- cypress/e2e/map-page.cy.ts
+++ cypress/e2e/map-page.cy.ts
@@ -5,4 +5,31 @@ describe("Map page", () => {
     cy.visit("#/map");
     cy.location("hash").should("match", /map\/[\d\.]+\/[\d\.]+\/\d+/);
   });
+
+  it("should open the detail page", () => {
+    cy.intercept("GET", "**/db.json", {
+      artworks: [
+        {
+          id: 1,
+          title: "Test",
+          year: 1,
+          tags: [],
+          town: "Town",
+          address: "Address",
+          latitude: 43.548747,
+          longitude: 10.721708,
+          authors: [],
+          authorIds: [],
+          images: [],
+          isAuthorUnknown: true,
+        },
+      ],
+      authors: [],
+    }).as("db");
+    cy.visit("#/map/43.548747/10.721708/18");
+    cy.wait("@db");
+    cy.contains("1").first().click();
+    cy.contains("Test").first().click();
+    cy.location("hash").should("match", /artworks\/1/);
+  });
 });
