998560bad1abecbb507ed70ab8c04ea8718b859f
Francesco <francesco.209@gmail.com>
2024-05-06 18:11:29 +0200

    Update artwork list e2e test

--- cypress/e2e/artwork-list-page.cy.ts
+++ cypress/e2e/artwork-list-page.cy.ts
@@ -6,8 +6,28 @@ describe("Artwork list page", () => {
   });
 
   it("should open the detail page", () => {
+    cy.intercept("GET", "**/db.json", {
+      artworks: [
+        {
+          id: 1,
+          title: "Test",
+          year: 1,
+          tags: [],
+          town: "Town",
+          address: "Address",
+          latitude: 43.548747,
+          longitude: 10.721708,
+          authors: [],
+          authorIds: [],
+          images: [],
+          isAuthorUnknown: true,
+        },
+      ],
+      authors: [],
+    }).as("db");
     cy.visit("#/artworks");
+    cy.wait("@db");
     cy.contains("Detail").first().click();
-    cy.location("hash").should("match", /artworks\/\d+/);
+    cy.location("hash").should("match", /artworks\/1/);
   });
 });
