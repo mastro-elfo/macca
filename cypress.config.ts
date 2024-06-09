98233c57574e03b6d68bd3d1cb51e2aff6426e03
Francesco <francesco.209@gmail.com>
2024-06-09 09:14:23 +0200

    Change `baseUrl` to work with vite preview

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "riamsw",
   e2e: {
-    baseUrl: "http://localhost:5173/macca",
+    baseUrl: "http://localhost:4173/macca",
     defaultCommandTimeout: 32000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
